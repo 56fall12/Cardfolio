df897df37d827f0b0cd49a029cc64f4a69af568f
Daniel Wang <daniw12@uci.edu>
2026-09-08 21:33:18 -0700

    Added the function to write to database for prices

---
+++ scraper/firebase_client.py
@@ -0,0 +1,15 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+from card import Card
+
+
+cred = credentials.Certificate("pokemon-card-collection-f9e21-firebase-adminsdk-fbsvc-41460bda94.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+def write_to_database(card: Card):
+
+    today_price: dict = {
+        "price" : card.price
+    }
+    db.collection("cards").document(str(card.id)).collection("price_history").document(card.date).set(today_price, merge = True)
